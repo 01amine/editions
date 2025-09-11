9609fa048d96bf2589fa622351221494818eb2cc
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-09-11 15:52:37 +0100

    updta ethe env and treafic fix everything ready for depoyement

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,7 +42,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*settings.ALLOWED_ORIGINS], 
+    allow_origins=[*settings.allowed_origins_list], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  
