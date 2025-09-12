fde3ab8b8ec411f3efb58d5a333c2e7f2884b0ca
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-09-12 01:34:40 +0100

    add landing page

---
+++ LandingPage/src/Components/Constants/index.js
@@ -0,0 +1,133 @@
+export const navLinks = [
+  {
+    id: "ourapp",
+    title: "ourapp",
+    aria_label : "Our App"
+  }
+  ,
+  {
+    id: "features",
+    title: "features",
+    aria_label : "Features"
+  },
+  {
+    id : "contactus",
+    title : "contactus",
+    aria_label : "Contact Us"
+   }
+];
+export const featureData = [
+  {
+    id : 1,
+    title : 'feature1',
+  },
+  {
+    id : 2,
+    title : 'feature2',
+  },
+  {
+    id : 3,
+    title : 'feature3',
+  },
+  {
+    id : 4,
+    title : 'feature4',
+  }
+];
+export const steps = [
+  {
+    number: 1,
+    title: 'step1',
+  },
+  {
+    number: 2,
+    title: 'step2',
+  },
+  {
+    number: 3,
+    title: 'step3',
+  },
+  {
+    number: 4,
+    title: 'step4',
+  },
+  {
+    number: 5,
+    title: 'step5',
+  },
+  {
+    number: 6,
+    title: 'step6',
+  }
+];
+
+export const footerLinks = [
+  {
+    title: "aboutus",
+    links: [
+      {
+        name: "link1",
+        name_arb : "من نحن",
+        link: "https://www.hoobank.com/Who-We-are/",
+      },
+      {
+        name: "link2",
+        name_arb : "محفظة",
+        link: "https://www.hoobank.com/Portfolio/",
+      },
+      {
+        name: "link3",
+        name_arb : "مشاريعنا",
+        link: "https://www.hoobank.com/Our-Projects/",
+      },
+      {
+        name: "link4",
+        name_arb : "شركاء",
+        link: "https://www.hoobank.com/Partners/",
+      },
+      {
+        name: "link5",
+        name_arb : "وسائل الإعلام",
+        link: "https://www.hoobank.com/Media/",
+      },
+    ],
+  },
+  {
+    title: "recherch",
+    title_arb : "عن ماذا تبحث ؟",
+    links: [
+      {
+        name: "link1",
+        name_arb : "خبراء الصحة",
+        link: "https://www.hoobank.com/Health-experts/",
+      },
+      {
+        name: "link2",
+        name_arb : "الأطباء",
+        link: "https://www.hoobank.com/Medcins/",
+      },
+      {
+        name: "link3",
+        name_arb : "مركز المساعدة",
+        link: "https://www.hoobank.com/Help-center/",
+      },
+      {
+        name: "link4",
+        name_arb : "المدونات",
+        link: "https://www.hoobank.com/Blogs/",
+      },
+      {
+        name: "link5",
+        name_arb : "اقتراحات",
+        link: "https://www.hoobank.com/Suggestions/",
+      },
+    ],
+  },
+];
+
+export const pricingLabels = {
+  monthly: "Monthly",
+  biannually: "Biannually",
+  annually: "Annually"
+};
+
