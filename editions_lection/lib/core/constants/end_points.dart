e3b1ac6fab192f8e4237be387a2e404f1f5becf8
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2025-09-17 13:33:13 +0100

    base url

--- editions_lection/lib/core/constants/end_points.dart
+++ editions_lection/lib/core/constants/end_points.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EndPoints {
-  static String baseUrl = dotenv.env['BASE_URL'] ??
-      'http://192.168.100.5:8000';
+  static String baseUrl =
+      dotenv.env['BASE_URL'] ?? 'https://api.lectionedition.com';
 }
